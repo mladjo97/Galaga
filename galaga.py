c652390477a2cf0800f7457044973ea9300e3fc8
Mladen Milosevic <mldnmilosevic@gmail.com>
2018-12-25 22:59:30 +0100

    Added some basic user interface for play/quit

--- galaga.py
+++ galaga.py
@@ -1,36 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
-from PyQt5.Qt import QIcon
-import config
+from PyQt5.QtWidgets import QApplication
+import gui
 import sys
-import game
-
-
-class Galaga(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        # initiates application UI
-        self.game = game.Game(1)
-        self.setCentralWidget(self.game)
-        self.setFixedSize(config.BOARD_WIDTH * config.TILE_WIDTH, config.BOARD_HEIGHT * config.TILE_HEIGHT)
-        self.center()
-
-        self.setWindowTitle('Galaga')
-        self.setWindowIcon(QIcon('images/ship.png'))
-        self.show()
-
-    def center(self):
-        # centers the window on the screen
-        screen = QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move((screen.width() - size.width()) / 2,
-                  (screen.height() - size.height()) / 2)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    galaga = Galaga()
+    game = gui.GameWindow()
     sys.exit(app.exec_())
