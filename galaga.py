d591019732a912d37d49ed2215c112a7651fb00c
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-01-16 14:59:22 +0100

    Preparing files for merge with labels branch

--- galaga.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-import gui
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    game = gui.GameWindow()
-    sys.exit(app.exec_())
